cfb71ac6aaab43cce021df6972ec3604a058c346
Ansh Kumar <140062016+Ansh8052@users.noreply.github.com>
2024-04-13 23:36:42 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const colors = require("colors");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://admin:deep1507@cluster0.rd0szsg.mongodb.net/');
+    await mongoose.connect('process.env.MONGO_URI');
     console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white);
   } catch (error) {
     console.log(`Mongodb Server Issue ${error}`.bgRed.white);
